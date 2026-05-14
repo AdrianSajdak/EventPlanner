7d4a459d2fc861df0f0ff312697ab2aca07a07ad
cutejunkie <obrzud.gabriela@gmail.com>
2026-05-14 18:32:12 +0200

    bottombar refactored - pamieta teraz co uzytkownik byl w trakcie robienia

--- src/navigation/MainNavigator.tsx
+++ src/navigation/MainNavigator.tsx
@@ -1,40 +1,37 @@
-import React, { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import EventsNavigator from './EventsNavigator';
 import FriendsNavigator from './FriendsNavigator';
 import NotificationsScreen from '../screens/notifications/NotificationsScreen';
 import ProfileNavigator from './ProfileNavigator';
-import { BottomNavBar, BottomTab } from '../components/common/BottomNavBar';
+import { BottomNavBar } from '../components/common/BottomNavBar';
+
+// typy dla zakładek 
+export type MainTabParamList = {
+  events: undefined;
+  friends: undefined;
+  notifications: undefined;
+  profile: undefined;
+};
+
+const Tab = createBottomTabNavigator<MainTabParamList>();
 
 export default function MainNavigator() {
-  const [activeTab, setActiveTab] = useState<BottomTab>('events');
-
-  const renderScreen = () => {
-    switch (activeTab) {
-      case 'events':
-        return <EventsNavigator />;
-      case 'friends':
-        return <FriendsNavigator />;
-      case 'notifications':
-        return <NotificationsScreen />;
-      case 'profile':
-        return <ProfileNavigator />;
-    }
-  };
-
   return (
-    <View style={styles.container}>
-      <View style={styles.content}>{renderScreen()}</View>
-      <BottomNavBar
-        activeTab={activeTab}
-        onTabPress={setActiveTab}
-        notificationCount={1}
-      />
-    </View>
+    <Tab.Navigator
+      tabBar={(props) => (
+        <BottomNavBar 
+          activeTab={props.state.routeNames[props.state.index] as any}
+          onTabPress={(tab) => props.navigation.navigate(tab)}
+          notificationCount={1} // zaciąga dane ze stanu/API
+        />
+      )}
+      screenOptions={{ headerShown: false }}
+    >
+      <Tab.Screen name="events" component={EventsNavigator} />
+      <Tab.Screen name="friends" component={FriendsNavigator} />
+      <Tab.Screen name="notifications" component={NotificationsScreen} />
+      <Tab.Screen name="profile" component={ProfileNavigator} />
+    </Tab.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: { flex: 1 },
-  content: { flex: 1 },
-});
